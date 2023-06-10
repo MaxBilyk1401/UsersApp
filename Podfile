eee0a9ab9a16a5a4f6d32e1a705b3c5f2e7ac8fe
MaxBilyk1401 <black.bro1401@gmail.com>
2023-06-10 21:22:48 +0300

    Installed pods, cleared files and deleted storyboard

---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '14.0'
+
+target 'UsersApp' do
+  use_frameworks!
+  pod 'SwiftLint'
+  pod 'R.swift'
+
+end
--- UsersApp/UsersViewController.swift
+++ UsersApp/UsersViewController.swift
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class UsersViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .cyan
     }
-
-
 }
-
